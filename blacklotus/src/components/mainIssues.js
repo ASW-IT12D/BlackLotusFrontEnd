62c2e118ecfbcb4f21f157a2a3c4acb0f102c145
Marc <marc.castro.chavez@estudiantat.upc.edu>
2023-05-24 17:52:11 +0200

    algo

--- blacklotus/src/components/mainIssues.js
+++ blacklotus/src/components/mainIssues.js
@@ -2,18 +2,19 @@ import React, { useEffect, useState } from 'react';
 
 function MainIssues() {
 
-    const [issues, setIssues] = useState([])
+  const token = "21bcd501e8c7eed561c6990c8e6fc2f6af84a0dd"
+  const [issues, setIssues] = useState([])
 
     useEffect(()=>{
         fetch("http://127.0.0.1:8000/issues/", {
         'method':'GET',
-        headers: {'Content-Type':'application/json','Authorization': 'Token 21bcd501e8c7eed561c6990c8e6fc2f6af84a0dd'}
+        headers: {'Content-Type':'application/json','Authorization': 'Token ' + token }
         }).then(resp => resp.json()).then(resp => setIssues(resp))
     }, [])
 
   return (
     <div>
-        { issues.map(issue => {
+        {issues.map(issue => {
             return <h2>{issue.subject}</h2>
             }
         )}
