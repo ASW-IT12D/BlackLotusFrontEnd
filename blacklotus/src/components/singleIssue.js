0b8fa9b933720cc18e00eb82a15473a6766c6792
pebese <paula.barrachina.c@gmail.com>
2023-05-28 21:31:12 +0200

    añadir comment sin css

--- blacklotus/src/components/singleIssue.js
+++ blacklotus/src/components/singleIssue.js
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
-import { changeUser, getToken } from '../Token';
+import { changeUser, getToken} from '../Token';
 
 function SingleIssue() {
-  const [subject, setSubject] = useState('');
-  const [description, setDescription] = useState('');
-  const [priority, setPriority] = useState('Low');
-  const [status, setStatus] = useState('New');
-  const [type, setType] = useState('Bug');
-  const [severity, setSeverity] = useState('Whishlist');
-
+  const [comment, setComment] = useState('');
+  const idIssue = 13;
   function getCookie(name) {
     let cookieValue = null;
 
@@ -30,51 +25,25 @@ function SingleIssue() {
 
     return cookieValue;
 }
+  
 
-
-  const handleSubjectChange = (event) => {
-    setSubject(event.target.value);
-  };
-
-  const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
-  };
-
-  const handlePriorityChange = (event) => {
-    setPriority(event.target.value);
-  };
-
-  const handleStatusChange = (event) => {
-    setStatus(event.target.value);
-  };
-
-  const handleTypeChange = (event) => {
-    setType(event.target.value);
-  };
-
-  const handleSeverityChange = (event) => {
-    setSeverity(event.target.value);
+  const handleComment = (event) => {
+    setComment(event.target.value);
   };
 
   changeUser()
 
   const handleButtonClick = () => {
     const data = {
-      subject: subject,
-      description: description,
-      status: status,
-      type: type,
-      severity: severity,
-      priority: priority
+      comment: comment,
     };
     
-
-    fetch('http://127.0.0.1:8000/issues/', {
+    fetch('http://127.0.0.1:8000/comment/'+idIssue+'/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'X-CSRFToken': getCookie('X_CSRFTOKEN'),
-            'Authorization': 'Token ' + getToken()
+            'Authorization': 'Token '+ getToken()
         },
         body: JSON.stringify(data),
     });
@@ -83,48 +52,8 @@ function SingleIssue() {
 
   return (
     <div>
-      <label>Subject: <input type="text" value={subject} onChange={handleSubjectChange} /></label>
-      <br></br>
-      <label>Description: <input type="text" value={description} onChange={handleDescriptionChange} /></label>
-      <br></br>
-      <label>Status:
-        <select value={status} onChange={handleStatusChange}>
-          <option value="New">New</option>
-          <option value="In progress">In progress</option>
-          <option value="Ready for test">Ready for test</option>
-          <option value="Closed">Closed</option>
-          <option value="Needs info">Needs info</option>
-          <option value="Rejected">Rejected</option>
-          <option value="Postponed">Postponed</option>
-        </select>
-      </label>
-      <br></br>
-      <label>Types:
-        <select value={type} onChange={handleTypeChange}>
-          <option value="Bug">Bug</option>
-          <option value="Question">Question</option>
-          <option value="Disables">Disables</option>
-        </select>
-      </label>
-      <br></br>
-      <label>Severity:
-        <select value={severity} onChange={handleSeverityChange}>
-          <option value="Whishlist">Whishlist</option>
-          <option value="Minor">Minor</option>
-          <option value="Normal">Normal</option>
-          <option value="Important">Important</option>
-          <option value="Critical">Critical</option>
-        </select>
-      </label>
-      <br></br>
-      <label>Priority:
-        <select value={priority} onChange={handlePriorityChange}>
-          <option value="Low">Low</option>
-          <option value="Medium">Medium</option>
-          <option value="High">High</option>
-        </select>
-      </label>
-      <br></br><button onClick={handleButtonClick}>Submit</button>
+      <label>Comment here: <input type="text" value={comment} onChange={handleComment} /></label>
+      <br></br><button onClick={handleButtonClick}>Comment</button>
     </div>
   );
 }
