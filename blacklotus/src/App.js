e5c68f535ae3b2e73f36101288ab8c8353f03215
Marc <marc.castro.chavez@estudiantat.upc.edu>
2023-06-01 00:52:23 +0200

    create done

--- blacklotus/src/App.js
+++ blacklotus/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/mainIssues';
 import Issue from './components/singleIssue';
 import Layout from './components/Layout';
+import CreaIssue from './components/creaIssue';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/issue' element={<Issue/>}/>
+          <Route path='/create-issue' element={<CreaIssue/>}/>
         </Routes>
       </BrowserRouter>
     </div>
